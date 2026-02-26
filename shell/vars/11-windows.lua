7dfd95ddd58a83b8e35f81ab9d793687929d4372
Cyprian Lenoir <Martinfischer533@gmail.com>
2026-02-26 21:09:01 +0100

    vars

--- shell/vars/11-windows.lua
+++ shell/vars/11-windows.lua
@@ -3,6 +3,7 @@ local vars = {
 	appdataLocal = { "${appdata}/Local" },
 	appdataRoaming = { "${appdata}/Roaming" },
 	ahk = { "${dotfiles}/ahk" },
+	ahkKeys = "${ahk}/generated/config.ini",
 	psProfile = { "${shellConfig}/generated/profile.ps1" },
 	systemPsProfile = { "C:/Windows/System32/WindowsPowerShell/v1.0/profile.ps1" },
 	startup = { "${appdataRoaming}/Microsoft/Windows/Start Menu/Programs/Startup" },
