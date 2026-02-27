8ba85a9029e5f3bf487008d178fd9278eac3a17c
cyp <martinfischer533@gmail.com>
2026-02-27 22:11:07 +0100

    fixed vars

--- shell/vars/21-personal.lua
+++ shell/vars/21-personal.lua
@@ -1,9 +1,12 @@
-return {
-	vdir = {
-		path = "${repos}/vdir.nvim",
-		dir_function = true,
-	},
-	learningGamification = {
-		path = "${repos}/LearningGamification",
-	},
+local vars = {
+	vdir = { "/vdir.nvim" },
+	learningGamification = { "/LearningGamification" },
+	regionLynx = { "/RegionLynx" },
 }
+
+for _, v in pairs(vars) do
+	v.dir_function = true
+	v[1] = "${repos}" .. v[1]
+end
+
+return vars
