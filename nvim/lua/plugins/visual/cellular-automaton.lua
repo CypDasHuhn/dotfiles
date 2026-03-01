2d6f0d6dcf84b49443a757518d5c650778d127de
cyp <martinfischer533@gmail.com>
2026-03-01 23:36:03 +0100

    stuff

---
+++ nvim/lua/plugins/visual/cellular-automaton.lua
@@ -0,0 +1,1 @@
+return { 'https://github.com/Eandrju/cellular-automaton.nvim' }
---
+++ nvim/lua/plugins/visual/dashboard.lua
@@ -0,0 +1,31 @@
+local function get_header()
+  local headers_dir = vim.fn.stdpath 'config' .. '/lua/plugins/visual/headers/'
+  local files = vim.fn.globpath(headers_dir, '*.txt', false, true)
+  math.randomseed(os.time())
+  local chosen = files[math.random(#files)]
+  local lines = {}
+  for line in io.lines(chosen) do
+    table.insert(lines, line)
+  end
+  return lines
+end
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      theme = 'hyper',
+      config = {
+        packages = { enable = true }, -- show how many plugins neovim loaded
+        -- limit how many projects list, action when you press key or enter it will run this action.
+        -- action can be a function type, e.g.
+        -- action = func(path) vim.cmd('Telescope find_files cwd=' .. path) end
+        project = { enable = true, limit = 8, icon = 'your icon', label = '', action = 'Telescope find_files cwd=' },
+        mru = { enable = true, limit = 10, icon = 'your icon', label = '', cwd_only = false },
+        footer = {}, -- footer
+        header = get_header(),
+      },
+    }
+  end,
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+}
