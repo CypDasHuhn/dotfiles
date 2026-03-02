be034fe3dda0e336cd5d6bd4898242a99c2938f4
c.lenoir <c.lenoir@isolutions.de>
2026-03-02 16:22:54 +0100

    c

--- shell/vars/11-windows.lua
+++ shell/vars/11-windows.lua
@@ -2,11 +2,15 @@ local vars = {
 	appdata = { "${me}/AppData" },
 	appdataLocal = { "${appdata}/Local" },
 	appdataRoaming = { "${appdata}/Roaming" },
+	programFiles = { "C:/Program Files" },
+
 	ahk = { "${dotfiles}/ahk" },
 	ahkKeys = { "${ahk}/generated/config.ini" },
 	psProfile = { "${shellConfig}/generated/profile.ps1" },
 	systemPsProfile = { "C:/Program Files/PowerShell/7/profile.ps1" },
+
 	startup = { "${appdataRoaming}/Microsoft/Windows/Start Menu/Programs/Startup" },
+
 	windowsTerminalConfig = {
 		"${dotfiles}/terminal-emulator/windows-terminal/generated.json",
 	},
--- shell/vars/22-work.lua
+++ shell/vars/22-work.lua
@@ -2,7 +2,6 @@ local vars = {
 	enerko = { "/i2s.EES.RedispatchPortal" },
 	usp = { "/BDEW_Umsatzportal" },
 	otis = { "/i2s.Bayer.OTIS" },
-	ihkDoku = { "/ihk-projekt-doku" },
 	sgProj = { "/i2s.SecGate.CentralUpdater" },
 }
 
@@ -10,5 +9,7 @@ for _, v in pairs(vars) do
 	v.dir_function = true
 	v[1] = "${repos}" .. v[1]
 end
+
+vars.ihkDoku = { "${vault}/02-work/ihk/projekt/06-Doku", dir_function = true }
 
 return vars
