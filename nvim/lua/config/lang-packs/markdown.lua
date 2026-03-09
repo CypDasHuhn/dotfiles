0b2c931cbb2677a1de8722cd2ac5932f7776fe35
c.lenoir <c.lenoir@isolutions.de>
2026-03-09 11:12:21 +0100

    termianl stuff

--- nvim/lua/config/lang-packs/markdown.lua
+++ nvim/lua/config/lang-packs/markdown.lua
@@ -1,6 +1,17 @@
 return {
   servers = {
     marksman = {},
+    plantuml_lsp = {
+      mason = false,
+      cmd = {
+        'plantuml-lsp',
+        '-jar-path',
+        'C:\\tools\\plantuml.jar',
+        '-stdlib-path',
+        'C:\\tools\\plantuml-stdlib\\stdlib',
+      },
+      filetypes = { 'plantuml' },
+    },
   },
   formatters = {},
   linters = {},
