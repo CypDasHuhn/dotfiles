897528282607dde549fbfd1480836097951be397
c.lenoir <martinfischer533@gmail.com>
2026-03-11 21:53:24 +0100

    lazy load

--- nvim/lua/plugins/stacks/dotnet/easy-dotnet.lua
+++ nvim/lua/plugins/stacks/dotnet/easy-dotnet.lua
@@ -1,10 +1,11 @@
 return {
-  "GustavEikaas/easy-dotnet.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
+  'GustavEikaas/easy-dotnet.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+  ft = { 'cs', 'fsproj', 'csproj', 'sln' },
   config = function()
-    require("easy-dotnet").setup({
+    require('easy-dotnet').setup {
       -- Use Roslyn LSP (default), not OmniSharp
-      lsp = "roslyn",
-    })
-  end
+      lsp = 'roslyn',
+    }
+  end,
 }
