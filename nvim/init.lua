0608385a8013429d6bb66ccbf6b7cb2a000301e5
Cyprian Lenoir <Martinfischer533@gmail.com>
2026-03-17 08:26:16 +0100

    structure rewrite

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,64 +1,9 @@
--- region Modules
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- Read ./explanation.md to understand the config files!
 
 require '.config'
--- endregion
 
--- region Filetypes
-vim.filetype.add {
-  pattern = {
-    ['.*/hypr/.*%.conf'] = 'hyprlang',
-    ['.*/hyprland/.*%.conf'] = 'hyprlang',
-  },
-}
--- endregion
+vim.opt.rtp:prepend(require('lib.lazy').lazypath)
 
--- region Yank
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
-})
--- endregion
-
--- region Lazy
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end
--- endregion
-
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
-
-require('lazy').setup({
+require('lazy').setup {
   { import = 'plugins' },
-}, {
-  -- region icons
-  ui = {
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-  -- endregion
-})
+}
