1996f64c780f617ba35bb7bbcfceea12433a51f0
cyp <martinfischer533@gmail.com>
2026-03-22 15:49:46 +0100

    gct

--- nvim/lua/plugins/misc/gct.lua
+++ nvim/lua/plugins/misc/gct.lua
@@ -1,24 +1,17 @@
 return {
   'CypDasHuhn/gct',
-  build = function()
-    local repo = vim.fn.stdpath 'data' .. '/lazy/gct/tree-sitter-gct'
-    local parser_dir = vim.fn.stdpath 'data' .. '/lazy/nvim-treesitter/parser'
-    vim.fn.system {
-      'gcc',
-      '-o',
-      repo .. '/gct.so',
-      '-shared',
-      '-fPIC',
-      '-O2',
-      '-I',
-      repo .. '/src',
-      repo .. '/src/parser.c',
-    }
-    vim.fn.system { 'cp', repo .. '/gct.so', parser_dir .. '/gct.so' }
-  end,
+  build = 'bash -c "cd tree-sitter-gct && tree-sitter generate && cc -shared -o parser/gct.so -fPIC -I./src src/parser.c -O2"',
   config = function()
+    local ts_path = vim.fn.stdpath 'data' .. '/lazy/gct/tree-sitter-gct'
+    vim.fn.mkdir(ts_path .. '/parser', 'p')
     vim.filetype.add { extension = { gct = 'gct' } }
+    vim.opt.rtp:append(ts_path)
 
-    vim.treesitter.language.register('gct', 'gct')
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'gct',
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
   end,
 }
