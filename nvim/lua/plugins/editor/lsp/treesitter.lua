a88bb3113201797a65a47f89a7d131149789e702
cyp <martinfischer533@gmail.com>
2026-03-28 11:40:06 +0100

    treesitter changes

--- nvim/lua/plugins/editor/lsp/treesitter.lua
+++ nvim/lua/plugins/editor/lsp/treesitter.lua
@@ -2,22 +2,27 @@ return {
   'nvim-treesitter/nvim-treesitter',
   event = { 'BufReadPost', 'BufNewFile' },
   build = ':TSUpdate',
-  main = 'nvim-treesitter.config', -- Sets main module to use for opts
-  opts = function()
+  config = function()
     local langs = require '.config.lang-packs.init'
 
-    return {
-      ensure_installed = vim.list_extend(
-        { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-        langs.treesitter
-      ),
-      auto_install = false,
-      ignore_install = { 'latex' },
-      highlight = {
-        enable = true,
-        disable = { 'latex' },
-      },
-      indent = { enable = true },
-    }
+    -- nvim-treesitter v1+ is a parser manager only; highlight/indent are Neovim built-ins
+    require('nvim-treesitter.config').setup {}
+
+    local parsers = vim.list_extend(
+      { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      langs.treesitter
+    )
+    parsers = vim.tbl_filter(function(p) return p ~= 'latex' end, parsers)
+    require('nvim-treesitter.install').install(parsers)
+
+    local hl_disabled = { latex = true }
+
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(ev)
+        if not hl_disabled[vim.bo[ev.buf].filetype] then
+          pcall(vim.treesitter.start, ev.buf)
+        end
+      end,
+    })
   end,
 }
