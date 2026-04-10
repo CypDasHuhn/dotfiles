cfba8e0ce29d45ff5f20031d9660c9de3160b034
c.lenoir <c.lenoir@isolutions.de>
2026-04-10 09:21:03 +0200

    dotnet

--- nvim/lua/plugins/stacks/dotnet/easy-dotnet.lua
+++ nvim/lua/plugins/stacks/dotnet/easy-dotnet.lua
@@ -4,8 +4,34 @@ return {
   ft = { 'cs', 'fsproj', 'csproj', 'sln' },
   config = function()
     require('easy-dotnet').setup {
-      -- Use Roslyn LSP (default), not OmniSharp
-      lsp = 'roslyn',
+      lsp = {
+        enabled = true,
+        auto_refresh_codelens = false,
+        config = {
+          settings = {
+            ["csharp|inlay_hints"] = {
+              csharp_enable_inlay_hints_for_implicit_object_creation = true,
+              csharp_enable_inlay_hints_for_implicit_variable_types = true,
+              csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+              csharp_enable_inlay_hints_for_types = true,
+              dotnet_enable_inlay_hints_for_parameters = true,
+            },
+            ["csharp|code_lens"] = {
+              dotnet_enable_references_code_lens = true,
+            },
+          },
+        },
+      },
     }
+
+    local csharp_codelens = require 'config.dotnet.codelens'
+    local easy_dotnet = vim.lsp.config.easy_dotnet
+    if easy_dotnet and easy_dotnet.on_attach then
+      local original_on_attach = easy_dotnet.on_attach
+      easy_dotnet.on_attach = function(client, bufnr)
+        original_on_attach(client, bufnr)
+        csharp_codelens.on_attach(client, bufnr)
+      end
+    end
   end,
 }
--- nvim/lua/plugins/stacks/git/diffview.lua
+++ nvim/lua/plugins/stacks/git/diffview.lua
@@ -1,4 +1,9 @@
 return {
-  'sindrets/diffview.nvim',
-  cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
+    'sindrets/diffview.nvim',
+    cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
+    keys = {
+        { '<leader>gco', '<cmd>DiffviewOpen<cr>',    desc = 'Open Diffview' },
+        { '<leader>gcx', '<cmd>DiffviewClose<cr>',   desc = 'Close Diffview' },
+        { '<leader>gcr', '<cmd>DiffviewRefresh<cr>', desc = 'Refresh Diffview' },
+    },
 }
