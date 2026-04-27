b0bac8fed8856c63f3dc744210367b7a71bba29f
Cyprian Lenoir <c.lenoir@i2solutions.de>
2026-04-27 11:13:24 +0200

    db grip

---
+++ nvim/lua/plugins/stacks/sql/dadbod-grip.lua
@@ -0,0 +1,15 @@
+return {
+  'joryeugene/dadbod-grip.nvim',
+  version = '*',
+  cmd = {
+    'Grip',
+    'GripStart',
+    'GripConnect',
+    'GripSchema',
+    'GripQuery',
+    'GripTables',
+    'GripAttach',
+    'GripOpen',
+    'GripExplain',
+  },
+}
