23be44d9a08436e8b3c51932e7507c9091f07d3d
Cyprian Lenoir <c.lenoir@i2solutions.de>
2026-05-04 09:09:43 +0200

    open with timeout

--- nvim/lua/config/binds.lua
+++ nvim/lua/config/binds.lua
@@ -86,3 +86,38 @@ vim.keymap.set('n', 'gg', 'gg0')
 vim.keymap.set('n', '<leader>i', ':Lazy<CR>', { noremap = true })
 
 vim.keymap.set({ 'n', 'v', 'o' }, '<leader>ts', '<cmd>set spell!<cr>', { desc = 'Toggle spell' })
+
+vim.keymap.set("n", "<leader>wn", "<cmd>noautocmd w<cr>")
+
+do
+    local function do_open(uri)
+        local cmd, err = vim.ui.open(uri)
+        local rv = cmd and cmd:wait(30000) or nil
+        if cmd and rv and rv.code ~= 0 then
+            err = ('vim.ui.open: command %s (%d): %s'):format(
+                (rv.code == 124 and 'timeout' or 'failed'),
+                rv.code,
+                vim.inspect(cmd.cmd)
+            )
+        end
+        return err
+    end
+
+    local gx_desc = 'Opens filepath or URI under cursor with the system handler (30s timeout)'
+    vim.keymap.set({ 'n' }, 'gx', function()
+        for _, url in ipairs(require('vim.ui')._get_urls()) do
+            local err = do_open(url)
+            if err then
+                vim.notify(err, vim.log.levels.ERROR)
+            end
+        end
+    end, { desc = gx_desc })
+
+    vim.keymap.set({ 'x' }, 'gx', function()
+        local lines = vim.fn.getregion(vim.fn.getpos('.'), vim.fn.getpos('v'), { type = vim.fn.mode() })
+        local err = do_open(table.concat(vim.iter(lines):map(vim.trim):totable()))
+        if err then
+            vim.notify(err, vim.log.levels.ERROR)
+        end
+    end, { desc = gx_desc })
+end
