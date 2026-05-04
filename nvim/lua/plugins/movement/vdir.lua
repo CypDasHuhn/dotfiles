934d8d0fef35f49f29c105cd734e639237fae193
Cyprian Lenoir <c.lenoir@i2solutions.de>
2026-05-04 08:45:35 +0200

    vdir ignore

--- nvim/lua/plugins/movement/vdir.lua
+++ nvim/lua/plugins/movement/vdir.lua
@@ -1,9 +1,20 @@
+local machine = {}
+local machine_path = vim.fn.stdpath('config') .. '/.machine.lua'
+
+local ok, machine_cfg = pcall(dofile, machine_path)
+if ok and type(machine_cfg) == 'table' then
+  machine = machine_cfg
+end
+
+local vdir_machine = machine.vdir or {}
+
 return {
-  dir = '~/repos/vdir.nvim',
+  dir = vdir_machine.dir or '~/repos/vdir.nvim',
   dependencies = {
     'nvim-neo-tree/neo-tree.nvim',
     'MunifTanjim/nui.nvim',
   },
+  enabled = vdir_machine.enabled == true,
   cmd = 'Vdir',
   keys = {
     { '<leader>q', '<cmd>Vdir<cr>', desc = 'Toggle Vdir' },
