e2c5a11934ee155f568ec52562250a3757ff2f48
Cyprian Lenoir <c.lenoir@i2solutions.de>
2026-05-13 16:31:23 +0200

    telescope functions

--- nvim/lua/plugins/movement/telescope.lua
+++ nvim/lua/plugins/movement/telescope.lua
@@ -36,10 +36,50 @@ return {
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sq', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch in existing [B]uffers' })
+
+    --region Directories Search
+    vim.keymap.set('n', '<leader>sd', function()
+      require('telescope.builtin').find_files({
+        find_command = { 'fd', '--type', 'd' },
+        prompt_title = 'Directories',
+        attach_mappings = function(prompt_bufnr, _map)
+          local actions = require('telescope.actions')
+          local action_state = require('telescope.actions.state')
+          actions.select_default:replace(function()
+            local entry = action_state.get_selected_entry()
+            actions.close(prompt_bufnr)
+            if not entry then return end
+            require('neo-tree.command').execute({
+              action = 'focus',
+              source = 'filesystem',
+              reveal_file = entry.path or entry.value,
+              reveal_force_cwd = false,
+            })
+          end)
+          return true
+        end,
+      })
+    end)
+    --endregion
+
+    --region Fuzzy search only files
+    local make_entry = require('telescope.make_entry')
+
+    vim.keymap.set('n', '<leader>sf', function()
+      require('telescope.builtin').find_files({
+        entry_maker = function(filepath)
+          local entry = make_entry.gen_from_file({})(filepath)
+          -- ordinal drives the matching, display drives what you see
+          entry.ordinal = vim.fn.fnamemodify(filepath, ':t')
+          return entry
+        end,
+      })
+    end)
+    --endregion
 
     vim.keymap.set('n', '<leader>/', function()
       builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -59,14 +99,5 @@ return {
     vim.keymap.set('n', '<leader>sn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
     end, { desc = '[S]earch [N]eovim files' })
-
-
-    vim.keymap.set('n', '<leader>sf', function()
-      vim.ui.input({ prompt = 'Grep in directory: ', completion = 'dir' }, function(dir)
-        if dir then
-          require('telescope.builtin').live_grep { search_dirs = { dir } }
-        end
-      end)
-    end)
   end,
 }
