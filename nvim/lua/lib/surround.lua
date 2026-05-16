577a015decceb297986154020e1348e5176cb1a4
lyprian <Martinfischer533@gmail.com>
2026-05-16 17:34:31 +0200

    format surround

--- nvim/lua/lib/surround.lua
+++ nvim/lua/lib/surround.lua
@@ -1,22 +1,27 @@
 local M = {}
 
 local pairs_map = {
-  ["{"] = { "{", "}" }, ["}"] = { "{", "}" },
-  ["("] = { "(", ")" }, [")"] = { "(", ")" },
-  ["["] = { "[", "]" }, ["]"] = { "[", "]" },
+  ['{'] = { '{', '}' },
+  ['}'] = { '{', '}' },
+  ['('] = { '(', ')' },
+  [')'] = { '(', ')' },
+  ['['] = { '[', ']' },
+  [']'] = { '[', ']' },
   ["'"] = { "'", "'" },
   ['"'] = { '"', '"' },
-  ["`"] = { "`", "`" },
+  ['`'] = { '`', '`' },
 }
 
 function M.surround_selection(char)
   local surround = pairs_map[char]
-  if not surround then return end
+  if not surround then
+    return
+  end
 
   local open, close = surround[1], surround[2]
 
-  local anchor = vim.fn.getpos("v")
-  local cursor = vim.fn.getpos(".")
+  local anchor = vim.fn.getpos 'v'
+  local cursor = vim.fn.getpos '.'
   local al, ac = anchor[2], anchor[3]
   local cl, cc = cursor[2], cursor[3]
 
@@ -29,28 +34,28 @@ function M.surround_selection(char)
     el, ec = al - 1, ac
   end
 
-  if vim.fn.mode() == "V" then
+  if vim.fn.mode() == 'V' then
     sc = 0
     ec = #vim.api.nvim_buf_get_lines(0, el, el + 1, false)[1]
   end
 
   vim.api.nvim_buf_set_text(0, el, ec, el, ec, { close })
   vim.api.nvim_buf_set_text(0, sl, sc, sl, sc, { open })
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', false)
 end
 
 function M.register_keymaps()
   local chars = {
-    ["{"] = "surround with {}",
-    ["("] = "surround with ()",
-    ["["] = "surround with []",
+    ['{'] = 'surround with {}',
+    ['('] = 'surround with ()',
+    ['['] = 'surround with []',
     ["'"] = "surround with ''",
     ['"'] = 'surround with ""',
-    ["`"] = "surround with ``",
+    ['`'] = 'surround with ``',
   }
-  vim.keymap.set("v", "r", "<Nop>", { desc = "+surround" })
+  vim.keymap.set('v', 'r', '<Nop>', { desc = '+surround' })
   for char, desc in pairs(chars) do
-    vim.keymap.set("v", "r" .. char, function()
+    vim.keymap.set('v', 'r' .. char, function()
       M.surround_selection(char)
     end, { desc = desc })
   end
