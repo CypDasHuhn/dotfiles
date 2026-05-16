f795d1e4fb36a5acbddf261c41ca950dba309b27
lyprian <Martinfischer533@gmail.com>
2026-05-16 17:34:39 +0200

    remove tree sitter dependencies

--- nvim/lua/plugins/stacks/sql/sql-ghostly.lua
+++ nvim/lua/plugins/stacks/sql/sql-ghostly.lua
@@ -1,9 +1,7 @@
 return {
   'pmouraguedes/sql-ghosty.nvim',
   ft = { 'sql' },
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-  },
+
   config = function()
     -- sql-ghosty hard-fails if the SQL parser is unavailable, so keep it
     -- disabled until Treesitter has the grammar installed on this machine.
