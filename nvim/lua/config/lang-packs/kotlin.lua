693b908581e8adf063ae07b5057dadec778b3437
Cyprian Lenoir <c.lenoir@i2solutions.de>
2026-06-18 09:58:11 +0200

    kotlin and treesitter

--- nvim/lua/config/lang-packs/kotlin.lua
+++ nvim/lua/config/lang-packs/kotlin.lua
@@ -1,18 +1,12 @@
 return {
   servers = {
-    --[[
     kotlin_lsp = {
       mason = false,
       single_file_support = false,
-      cmd = { 'kotlin-lsp-nvim', '--stdio' },
+      cmd = { 'kotlin-lsp', '--stdio' },
       init_options = {
         storagePath = vim.fn.stdpath 'data' .. '/kotlin-lsp',
       },
-    },
-    --]]
-    kotlin_language_server = {
-      mason = false,
-      cmd = { 'kotlin-language-server-nvim' },
       on_attach = function(client)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
--- nvim/lua/plugins/editor/treesitter.lua
+++ nvim/lua/plugins/editor/treesitter.lua
@@ -9,31 +9,18 @@ return {
   init = function()
     vim.opt.runtimepath:prepend(parser_dir)
   end,
-  opts = {
-    install_dir = parser_dir,
-  },
-  config = function(_, opts)
-    require('nvim-treesitter.config').setup(opts)
-
-    local install = require 'nvim-treesitter.install'
-
+  opts = function()
     local ensure_installed = {}
     for _, lang in ipairs(langs.treesitter or {}) do
       ensure_installed[lang] = true
     end
-    local languages = vim.tbl_keys(ensure_installed)
-
-    if #languages > 0 then
-      install.install(languages)
-    end
-
-    vim.api.nvim_create_autocmd('FileType', {
-      group = vim.api.nvim_create_augroup('TSEnsureInstalled', { clear = true }),
-      callback = function(args)
-        if langs.treesitter and vim.list_contains(langs.treesitter, vim.bo[args.buf].filetype) then
-          install.install(vim.bo[args.buf].filetype)
-        end
-      end,
-    })
+    return {
+      parser_install_dir = parser_dir,
+      ensure_installed = vim.tbl_keys(ensure_installed),
+      auto_install = true,
+    }
+  end,
+  config = function(_, opts)
+    require('nvim-treesitter.configs').setup(opts)
   end,
 }
