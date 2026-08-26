dd36efa4e2e7412afe938bfc54204df360a8b241
Cyprian Lenoir <c.lenoir@i2solutions.de>
2026-08-26 14:04:53 +0200

    azure work

---
+++ nvim/lua/plugins/misc/adopure.lua
@@ -0,0 +1,11 @@
+return {
+    "Willem-J-an/adopure.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+        "sindrets/diffview.nvim"
+    },
+    config = function()
+        vim.g.adopure = {}
+    end,
+}
