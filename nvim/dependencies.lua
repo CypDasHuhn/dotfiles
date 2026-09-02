4bd040ba250e81821d0dbffd104492573f563f8d
c.lenoir <martinfischer533@gmail.com>
2026-09-02 21:47:38 +0200

    add latex support

--- nvim/dependencies.lua
+++ nvim/dependencies.lua
@@ -8,4 +8,9 @@ return {
         unix = h.pacman 'markdownlint-cli2',
         windows = h.npm_pkg 'markdownlint-cli2',
     }):once(),
+    -- LaTeX -> unicode converter for render-markdown.nvim (opts.latex.converter = latex2text)
+    pylatexenc = h.dep({
+        unix = h.pacman('python-pylatexenc', 'latex2text'),
+        windows = h.pipx_pkg('pylatexenc', 'latex2text'),
+    }):once(),
 }
