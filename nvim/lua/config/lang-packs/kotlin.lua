7180c115476a361e7a16c10badfa6757534f2cde
cyp <martinfischer533@gmail.com>
2026-09-04 22:22:11 +0200

    feat: kotlin new lsp

--- nvim/lua/config/lang-packs/kotlin.lua
+++ nvim/lua/config/lang-packs/kotlin.lua
@@ -3,14 +3,15 @@ return {
     kotlin_lsp = {
       mason = false,
       single_file_support = false,
-      cmd = { 'kotlin-lsp', '--stdio' },
-      init_options = {
-        storagePath = vim.fn.stdpath 'data' .. '/kotlin-lsp',
+      cmd = {
+        'kotlin-lsp',
+        '--stdio',
+        '--system-path',
+        vim.fn.stdpath 'data' .. '/kotlin-lsp',
       },
       on_attach = function(client)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
-        client.server_capabilities.documentHighlightProvider = false
       end,
     },
   },
