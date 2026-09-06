5c2bbe92f1bdf67842f5adbe1982eb9464a36aa7
cyp <martinfischer533@gmail.com>
2026-09-06 14:32:47 +0200

    fix(nvim): update lsp keybinds for removed Lsp* commands

--- nvim/lua/config/binds/lsp-diagnostics.lua
+++ nvim/lua/config/binds/lsp-diagnostics.lua
@@ -1,6 +1,8 @@
-vim.keymap.set('n', '<leader>glr', ':LspRestart<cr>')
-vim.keymap.set('n', '<leader>glg', ':LspLog<cr>')
-vim.keymap.set('n', '<leader>gli', ':LspInfo<cr>')
+vim.keymap.set('n', '<leader>glr', '<cmd>lsp restart<cr>', { desc = 'Restart LSP client(s)' })
+vim.keymap.set('n', '<leader>glg', function()
+    vim.cmd('tabnew ' .. vim.lsp.log.get_filename())
+end, { desc = 'Open LSP log' })
+vim.keymap.set('n', '<leader>gli', '<cmd>checkhealth vim.lsp<cr>', { desc = 'LSP info' })
 
 vim.keymap.set('n', '<leader>xq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('n', '<leader>td', function()
