9424f67a23400efa17827c4fc284c42c8d8df841
cyp <martinfischer533@gmail.com>
2026-09-07 22:57:14 +0200

    fix(lsp): kotlin auto build system

--- nvim/lua/config/lang-packs/kotlin.lua
+++ nvim/lua/config/lang-packs/kotlin.lua
@@ -3,6 +3,11 @@ return {
     kotlin_lsp = {
       mason = false,
       single_file_support = false,
+      init_options = {
+        projects = {
+          { type = 'jps', path = '$PROJECT_DIR$' },
+        },
+      },
       cmd = {
         'kotlin-lsp',
         '--stdio',
