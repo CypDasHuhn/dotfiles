3af29656706f1e22bb00669a2ae4c1dd2fde2db1
cyp <martinfischer533@gmail.com>
2026-09-08 22:02:56 +0200

    feat(shell): add fcp install path

--- shell/paths/10-unix.lua
+++ shell/paths/10-unix.lua
@@ -4,6 +4,7 @@ local paths = {
 	{ "${me}/.zvm/bin" },
 	{ "${me}/.zvm/self" },
 	{ "${me}/.zvm/master" },
+	{ "${me}/repos/fcp/build/install/fcp/bin" },
 	{ "${me}/.cargo/bin" },
 }
 paths.only = { os = "unix" }
