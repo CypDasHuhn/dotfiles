cd1f5a8b9a4be9e14a6c5d8b769a0266de06baac
cyp <martinfischer533@gmail.com>
2026-09-12 16:36:08 +0200

    revert(nvim): restore generic r visual surround mapping

--- nvim/lua/plugins/editor/typing/surround.lua
+++ nvim/lua/plugins/editor/typing/surround.lua
@@ -7,17 +7,6 @@ return {
   opts = {},
   config = function(_, opts)
     require('nvim-surround').setup(opts)
-    local surrounds = {
-      { '(', ')' },
-      { '[', ']' },
-      { '{', '}' },
-      { "'", "'" },
-      { '"', '"' },
-    }
-    for _, pair in ipairs(surrounds) do
-      vim.keymap.set('x', pair[1], '<Plug>(nvim-surround-visual)' .. pair[1], {
-        desc = 'Surround selection with ' .. pair[1] .. pair[2],
-      })
-    end
+    vim.keymap.set('x', 'r', '<Plug>(nvim-surround-visual)', { desc = 'Surround selection' })
   end,
 }
