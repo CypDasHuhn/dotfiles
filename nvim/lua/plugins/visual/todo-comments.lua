8b59f6072867b5e1c2df324229fd4718f3420e7c
cyp <martinfischer533@gmail.com>
2026-09-13 11:41:38 +0200

    feat(nvim): wire todo-comments jumps and pickers
    Add ]T/[T jumps plus TodoTelescope, TodoTrouble and TodoQuickFix bindings
    under the search and issues groups.

--- nvim/lua/plugins/visual/todo-comments.lua
+++ nvim/lua/plugins/visual/todo-comments.lua
@@ -1,1 +1,15 @@
-return { 'folke/todo-comments.nvim', event = 'VeryLazy', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+return {
+  'folke/todo-comments.nvim',
+  event = 'VeryLazy',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = { signs = false },
+  config = function(_, opts)
+    require('todo-comments').setup(opts)
+
+    --region Pickers
+    vim.keymap.set('n', '<leader>st', '<cmd>TodoTelescope<cr>', { desc = '[S]earch [T]odos' })
+    vim.keymap.set('n', '<leader>xt', '<cmd>TodoTrouble<cr>', { desc = '[T]odos (Trouble)' })
+    vim.keymap.set('n', '<leader>xT', '<cmd>TodoQuickFix<cr>', { desc = '[T]odos (QuickFix)' })
+    --endregion
+  end,
+}
